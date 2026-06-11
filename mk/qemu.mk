33aa4ecf4fb54e58842c4f30474d89f46a228956
Rafid Islam <goku237411@gmail.com>
2026-06-11 14:18:58 +0600

    build: add advanced QEMU logging and debug targets

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -2,4 +2,10 @@
 # Run
 #
 run: $(BUILD_DIR)/main_floppy.img
-	qemu-system-i386 -fda $(BUILD_DIR)/main_floppy.img
+	qemu-system-i386 -d int,cpu_reset -D qemu.log -no-reboot -no-shutdown -fda build/main_floppy.img
+
+#
+# Debug
+#
+debug: $(BUILD_DIR)/main_floppy.img
+	qemu-system-i386 -d int,cpu_reset -D qemu.log -no-reboot -no-shutdown -fda build/main_floppy.img -s -S
