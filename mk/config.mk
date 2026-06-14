a5d3a6232b7a93137cb66990b7cf92155a79eada
Rafid Islam <goku237411@gmail.com>
2026-06-14 13:00:10 +0600

    build: improve kernel build system

--- mk/config.mk
+++ mk/config.mk
@@ -4,13 +4,13 @@
 
 AS := nasm
 CC := i686-elf-gcc
-LD := i686-elf-gcc        # using gcc as linker driver
+LD := i686-elf-gcc    # using gcc as linker driver
 DD := dd
-OBJCOPY :=
+OBJCOPY := objcopy
 
 ASFLAGS := -f bin
 WARNFLAGS := -Wall -Wextra -Werror=strict-prototypes -Wold-style-definition -Wundef -Wwrite-strings -Wpointer-arith
-CFLAGS := -ffreestanding -fno-builtin -fno-pie -fno-stack-protector -mno-mmx -mno-sse -nostdlib -nostdinc -I include/ -c
+CFLAGS := -g -ffreestanding -fno-builtin -fno-pie -fno-stack-protector -mno-mmx -mno-sse -nostdlib -nostdinc -I include/ -c
 CFLAGS +=
 ASFLAGS_ELF := -f elf32
 
@@ -24,12 +24,12 @@ SRC := src
 BOOTLOADER_DIR := src/bootloader
 STAGE1_ASM_SRC := $(wildcard $(BOOTLOADER_DIR)/stage1/*.asm)
 STAGE1_5_ASM_SRC := $(wildcard $(BOOTLOADER_DIR)/stage1.5/*.asm)
-KERNEL_ASM_SRC := $(wildcard $(SRC)/kernel/*.asm)
-KERNEL_C_SRC := $(wildcard $(SRC)/kernel/*.c)
+KERNEL_ASM_SRC := $(shell find $(SRC)/kernel/ -type f -name "*.asm")
+KERNEL_C_SRC := $(shell find $(SRC)/kernel/ -type f -name "*.c")
 BOOTLOADER_BINS := $(BUILD_DIR)/stage1.bin $(BUILD_DIR)/stage1-5.bin
-KERNEL_C_OBJS := $(patsubst $(SRC)/%, $(BUILD_DIR)/%, $(KERNEL_C_SRC:.c=.o))
-KERNEL_ASM_OBJS := $(patsubst $(SRC)/%, $(BUILD_DIR)/%, $(KERNEL_ASM_SRC:.asm=.o))
-KERNEL_OBJS := $(KERNEL_OBJS) $(KERNEL_ASM_OBJS)
+KERNEL_C_OBJS := $(patsubst $(SRC)/kernel/%.c, $(BUILD_DIR)/kernel/c/%.o, $(KERNEL_C_SRC))
+KERNEL_ASM_OBJS := $(patsubst $(SRC)/kernel/%.asm, $(BUILD_DIR)/kernel/asm/%.o, $(KERNEL_ASM_SRC))
+KERNEL_OBJS := $(KERNEL_C_OBJS) $(KERNEL_ASM_OBJS)
 
 #
 # Macro
--- mk/kernel.mk
+++ mk/kernel.mk
@@ -4,13 +4,15 @@
 kernel: $(BUILD_DIR)/kernel.bin
 
 
-$(BUILD_DIR)/kernel.bin: $(KERNEL_OBJS) | always
+$(BUILD_DIR)/kernel.bin: $(KERNEL_OBJS) src/kernel/linker.ld | always
 	$(LD) -nostdlib -T src/kernel/linker.ld -o $(BUILD_DIR)/kernel.elf $(KERNEL_OBJS)
-	objcopy -O binary $(BUILD_DIR)/kernel.elf $@
+	$(OBJCOPY) -O binary $(BUILD_DIR)/kernel.elf $@
 
 
-$(BUILD_DIR)/kernel/%.o: $(SRC)/kernel/%.c | always
+$(BUILD_DIR)/kernel/c/%.o: $(SRC)/kernel/%.c | always
+	@mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) $< -o $@
 
-$(BUILD_DIR)/kernel/%.o: $(SRC)/kernel/%.asm | always
+$(BUILD_DIR)/kernel/asm/%.o: $(SRC)/kernel/%.asm | always
+	@mkdir -p $(dir $@)
 	$(AS) $(ASFLAGS_ELF) $< -o $@
